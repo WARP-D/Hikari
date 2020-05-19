d07ce88b8bd084cfee729a4bfd39eb695d4e6de2
SoRA_X7 <galaxycomet2002@gmail.com>
2020-05-19 16:47:07 +0900

    Remove Physics

--- Assets/AI/Node.cs
+++ Assets/AI/Node.cs
@@ -1,5 +1,3 @@
-using Unity.Collections;
-
 namespace Cyanite.AI {
     public struct Node {
         public bool valid;
@@ -23,16 +21,6 @@ namespace Cyanite.AI {
             children = default;
             eval = evaluation;
             holdUsed = false;
-        }
-
-        public NativeArray<IndexedNode> GetChildren(NativeArray<Node> tree, Allocator alloc = Allocator.Temp) {
-            var array = new NativeArray<IndexedNode>(children.length,alloc);
-            var treeSlice = new NativeSlice<Node>(tree,children.start,children.length);
-            for (var i = 0; i < children.length; i++) {
-                array[i] = new IndexedNode(children.start + i, treeSlice[i]);
-            }
-
-            return array;
         }
     }
 }
